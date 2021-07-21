f2a5f8be218fded1f79ef63ff0edda9eb40f4fdd
FT-Fetters <1438255191@qq.com>
2021-07-21 20:42:22 +0800

    添加按钮和文件列表栏两个空间的类

---
+++ src/lyun/longzhi/components/Button.java
@@ -0,0 +1,65 @@
+package lyun.longzhi.components;
+
+import java.awt.*;
+
+public class Button implements Component{
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void setX(int x) {
+
+    }
+
+    @Override
+    public void setY(int y) {
+
+    }
+
+    @Override
+    public void setBorder(Color color, int width) {
+
+    }
+
+    @Override
+    public void removeBorder() {
+
+    }
+
+    @Override
+    public void setBackground(Color color) {
+
+    }
+
+    @Override
+    public void removeBackground() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+}
