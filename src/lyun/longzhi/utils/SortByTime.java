040c51548b4f01aefe95150ce92c296b9cc4ce50
FT-Fetters <1438255191@qq.com>
2021-07-31 19:47:56 +0800

    创建SortByTime工具类

---
+++ src/lyun/longzhi/utils/SortByTime.java
@@ -0,0 +1,20 @@
+package lyun.longzhi.utils;
+
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 设计一个能获取指定目录下的所有文件,并且按照文件的修改时间分类的方法
+ */
+public class SortByTime {
+    /**
+     * 获取该目录下所有文件的时间分类图
+     * @param path 要获取的目录
+     * @return 分类图图
+     */
+    public static HashMap<Date, List<File>> sort(String path){
+        return null;
+    }
+}
