dda1a40ca39b53e52dc27c7f771e523b64380f6b
DMH695 <2313978908@qq.com>
2021-08-25 16:06:56 +0800

    新建

---
+++ src/lyun/longzhi/components/CustomizeView.java
@@ -0,0 +1,111 @@
+package lyun.longzhi.components;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class CustomizeView implements Component{
+    @Override
+    public void setEnable(boolean enable) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void setX(int x) {
+
+    }
+
+    @Override
+    public void setY(int y) {
+
+    }
+
+    @Override
+    public void setBorder(Color color, int width) {
+
+    }
+
+    @Override
+    public void removeBorder() {
+
+    }
+
+    @Override
+    public void setBackground(Color color) {
+
+    }
+
+    @Override
+    public void removeBackground() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void mouseClick(int x, int y) {
+
+    }
+
+    @Override
+    public void mouseEnter() {
+
+    }
+
+    @Override
+    public void mouseLeave() {
+
+    }
+
+    @Override
+    public void mouseMove(int x, int y) {
+
+    }
+
+    @Override
+    public void mouseDoubleClick(int x, int y) throws IOException {
+
+    }
+
+    @Override
+    public void mousePress(int x, int y) {
+
+    }
+
+    @Override
+    public void mouseRelease(int x, int y) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(int wheel) {
+
+    }
+}
