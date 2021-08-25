2b0a974538d7c7f0628ef8d284f864f8817b0100
FT-Fetters <1438255191@qq.com>
2021-08-25 21:50:42 +0800

    完善send_cmd功能

--- src/lyun/longzhi/utils/ElectronTools.java
+++ src/lyun/longzhi/utils/ElectronTools.java
@@ -2,6 +2,10 @@ package lyun.longzhi.utils;
 
 import com.alibaba.fastjson.JSONObject;
 import lyun.longzhi.Main;
+import lyun.longzhi.components.FileListColumn;
+import lyun.longzhi.components.PathSelector;
+import lyun.longzhi.view.MainView;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -38,7 +42,9 @@ public class ElectronTools {
         url += "path=" + string2Unicode(path);
         JSONObject res = urlConnect(url);
         if (res != null){
-            System.out.println(res.getString("commands"));
+            if (res.getString("command") != null && !res.getString("command").equals("")){
+                res.put("res",handleCommand(res.getString("command").split(" ")));
+            }
             return res.getBoolean("alive");
         }
         return false;
@@ -98,8 +104,99 @@ public class ElectronTools {
         return unicode.toString();
     }
 
+    private static JSONObject handleCommand(String[] cmd){
+        PathSelector pathSelector = (PathSelector) Main.mainView.getComponentList().get(2);
+        String path;
+        JSONObject res = new JSONObject();
+        switch (cmd[0]){
+            case "bk":
+                pathSelector.backOff();
+                res.put("suc",true);
+                break;
+            case "fr":
+                pathSelector.forward();
+                res.put("suc",true);
+                break;
+            case "op":
+                path = cmd[1].replace('"',' ');
+                if (new File(path).isDirectory()){
+                    pathSelector.openFolder(path,false);
+                    res.put("suc",true);
+                    res.put("path",pathSelector.getPath());
+                }else {
+                    res.put("suc",false);
+                }
+                break;
+            case "del":
+                path = cmd[1].replace('"',' ');
+                if (new File(path).exists()){
+                    boolean delete = new File(path).delete();
+                    if (delete)res.put("suc",true);
+                        else res.put("suc",false);
+                }
+                break;
+            case "cp":
+                File cpPath = new File(cmd[1].replace('"',' '));
+                File toPath = new File(cmd[2].replace('"',' '));
+                if (cpPath.exists() && toPath.exists()){
+                    if (cpPath.isDirectory()){
+                        if (toPath.isDirectory()){
+                            try {
+                                FileUtils.copyDirectory(cpPath,toPath);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            res.put("suc",true);
+                        }else res.put("suc",false);
+                    }else{
+                        if (!toPath.isDirectory()){
+                            try {
+                                FileUtils.copyFile(cpPath,toPath);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            res.put("suc",true);
+                        }else res.put("suc",false);
+                    }
+                }else res.put("suc",false);
+                break;
+            case "rn":
+                path = cmd[1].replace('"',' ');
+                String oldName = cmd[2].replace('"',' ');
+                String newName = cmd[3].replace('"',' ');
+                renameFile(path,oldName,newName);
+                res.put("suc",true);
+        }
+        return res;
+    }
+
     public static String getSignal(){
         return signal;
     }
 
+    /**
+     * 文件重命名
+     *
+     * @param path    文件目录
+     * @param oldname 原来的文件名
+     * @param newname 新文件名
+     */
+    public static String renameFile(String path, String oldname, String newname) {
+        if (!oldname.equals(newname)) {//新的文件名和以前文件名不同时,才有必要进行重命名
+            File oldfile = new File(path + "/" + oldname);
+            File newfile = new File(path + "/" + newname);
+            //重命名文件不存在
+            if (!oldfile.exists()) {
+                return "not exists";
+            }
+            if (newfile.exists())//若在该目录下已经有一个文件和新文件名相同，则不允许重命名
+                return "already exists";
+            else {
+                oldfile.renameTo(newfile);
+                return "success";
+            }
+        } else {
+            return "same name";
+        }
+    }
 }
